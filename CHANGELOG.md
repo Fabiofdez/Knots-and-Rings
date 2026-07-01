4bd52b1fec178db8cc016ea629f43055a76173d6
Fabio Fernandez <ffernan9@jhu.edu>
2026-06-30 21:23:59 -0500

    v1.3.2 - Regions Unexplored support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,2 @@
 ### Additions
-- Now works with Fusion
-- Mod auto-detects Fusion and loads needed resources
-
-### Changes
-- Resource pack now supports more formats
-- Mod resources new enabled by default
-
-### Fixes
-- Stripped wood blocks now have overlay edges
-- Remove unused CTM assets for stripped log ends
+- Now supports Regions Unexplored wood types
--- src/main/java/fabiofdez/knots_and_rings/KnotsAndRings.java
+++ src/main/java/fabiofdez/knots_and_rings/KnotsAndRings.java
@@ -18,7 +18,7 @@ import fabiofdez.knots_and_rings.platform.fabric.FabricPlatform;
 public class KnotsAndRings {
 
 	public static final String MOD_ID = /*$ mod_id*/ "knots_and_rings";
-	public static final String MOD_VERSION = /*$ mod_version*/ "1.3.1";
+	public static final String MOD_VERSION = /*$ mod_version*/ "1.3.2";
 	public static final String MOD_FRIENDLY_NAME = /*$ mod_name*/ "Knots & Rings";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
